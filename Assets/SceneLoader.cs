be7841cda8afa6953e23abc525d6a10134763006
arnaudkrafft <arnaud.krafft@telecomnancy.net>
2025-09-21 14:11:25 +0200

    FixedLoadingScreen

--- Assets/SceneLoader.cs
+++ Assets/SceneLoader.cs
@@ -4,14 +4,28 @@ using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour
 {
     private string SceneToToad;
-
+    public GameObject loadingCanvas;
+    private int framestoloading;
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (SceneManager.GetActiveScene().name == "LoadingScene")
         {
+            framestoloading = 5;
             SceneManager.LoadScene(SceneToToad);
             SceneToToad = "";
+            loadingCanvas.SetActive(true);
+        }
+        else
+        {
+            if(framestoloading >5)
+            {
+                framestoloading--;
+            }
+            else
+            {
+                loadingCanvas.SetActive(false);
+            }
         }
     }
 
